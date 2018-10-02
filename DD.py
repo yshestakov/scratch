674653a0046c0006556d661b839b722256d298e5
Yuriy Shestakov <yshestakov@gmail.com>
2018-10-02 10:32:56 +0300

    Updated DD.py

--- DD.py
+++ DD.py
@@ -2,9 +2,19 @@ from threading import Thread
 from subprocess import PIPE, Popen
 from time import time
 
+from logging import basicConfig, info, ERROR, error
+basicConfig(level=ERROR, format="%(asctime)s - [%(levelname)s] [%(threadName)s]"
+                                " (%(module)s:%(lineno)d) %(message)s", filename="errors.log")
+
+
+'''
+  Define a Thread-class to run the tasks in parallel.
+  Vars description are available in their input prompt
+'''
 
 class MyThread(Thread):
     def __init__(self, file_name, file_size, count, t_number):
+        info("Initializing thread %s to execute..." % t_number)
         Thread.__init__(self)
         super(MyThread, self).__init__()
         self.name = file_name
@@ -19,7 +29,8 @@ class MyThread(Thread):
                    "count="+self.count, "of="+self.name], stderr=PIPE)
 
             print("Time to execute thread #%s: " % self.number, time() - start_time)
-        except:
+        except Exception:
+            error("Oh, no! You gotta check the input values and/or run-function")
             exit(1)
 
 
@@ -27,13 +38,15 @@ def check_disk(min_size):
     try:
         disks = []
         disks_list = Popen(["df", "-lBM", "--output=source,avail"], stdout=PIPE)
+        info("Collecting info about disks and available space are completed!")
 
         for line in disks_list.stdout.readlines():
             if line.startswith("/dev"):
                 size = int(line.strip().split()[-1].replace('M', ''))
                 if size > min_size:
                     disks.append(line.strip().split()[0])
-    except:
+    except Exception:
+        error("Oh, no! Collecting disks info failed!")
         raise exit(1)
 
     return disks
@@ -44,15 +57,17 @@ def create_files(file_name, file_size, count):
         for i in range(1, count+1):
             with open(file_name + str(i), "wb") as file:
                 file.write(b"\0" * 1024 * 1024 * file_size)
-    except:
+                info("File %s has been created successfully" % file_name+str(i))
+    except Exception:
+        error("Oh, no! Files couldn't be created!")
         exit(1)
 
 
 def main():
-    count = 10
-    disk_min = 500
-    file_size = 10
-    file_name = 'file'
+    count = int(input("Please, enter the number of threads: "))
+    disk_min = int(input("Please, enter the minimum size of disk free space: "))
+    file_size = int(input("Please, enter the size of files to create: "))
+    file_name = input("Please, enter a prefix for file names: ")
 
     check_disk(disk_min)
     create_files(file_name, file_size, count)
@@ -61,6 +76,7 @@ def main():
     for i in range(1, count+1):
         threads.append(MyThread(file_name + str(i), file_size, count, i))
 
+    info("Let's go with multi-threading!")
     for t in threads:
         t.start()
 
